5f58d657b24aeea27d1f1410964cb7516196185d
roshannizar <roshannizar8@gmail.com>
2019-07-11 16:39:06 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const Bundler = require('parcel-bundler'),
+      express = require('express'),
+      mongoose = require('mongoose');
+
+const bundler = new Bundler('./public/index.html', {});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bundler.middleware());
+
+app.use(express.static('./dist'));
+
+app.get('/', function(req, res) {
+    res.sendFile('./dist/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, err => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(`LawDoc is running on port ${port}`);
+});
