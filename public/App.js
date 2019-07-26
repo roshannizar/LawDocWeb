a20517295ce0a77ad90ae80cd437f03c892448f6
roshannizar <roshannizar8@gmail.com>
2019-07-26 14:25:12 +0530

    Update

--- public/App.js
+++ public/App.js
@@ -1,37 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { NavDropdown, Nav } from 'react-bootstrap';
 
 import './App.css';
+import Pic from './icons/person.png';
+import style from 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
 
-    render() {
-        return(
-            <div>
-                <div className="navi-top">
-                    <div className="navi-top-link left">
-                        <form>
-                            <input type="text" name="searchbar" className="search-bar" placeholder="Search"/>
-                        </form>
-                    </div>
-                    <div className="navi-top-link right slight-padding">
-                        <a href="">Profile</a>
-                    </div>
+  render() {
+    return (
+      <div>
+        <div className="nav-bar">
+          <div className="nav-brand">
+            <label className="brand">LawDoc</label>
+          </div>
+          <div className="link-bar">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home" className="link-bar-color">Notification</Nav.Link>
+              <Nav.Link href="#link" className="link-bar-color">Support</Nav.Link>
+              <NavDropdown title="Mark Otto" id="basic-nav-dropdown" className="link-bar-color">
+                <div className="person">
+                  <img src={Pic} className="person-img" />
                 </div>
-                <div className="side-nav">
-                    <div className="heading">
-                        <label className="topic">LAWDOC</label>
-                    </div>
-                    <div className="btn-container">
-                        <label className="container-heading">SERVICES</label>
-                        <div className="btns">
-                            <button className="btn">Dashboard</button><br/>
-                            <button className="btn">My Documents</button>
-                        </div>                       
-                    </div>
-                </div>
-            </div>
-        );
-    }
+                <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Settings</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Switch User</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.5">Sign Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
